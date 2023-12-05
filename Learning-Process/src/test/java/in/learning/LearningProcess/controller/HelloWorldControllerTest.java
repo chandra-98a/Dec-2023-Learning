bbbba320df7f2208aed71a336692c9e5eab47ae6
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-05 13:21:29 +0530

    "test case for HelloWorldController class"

---
+++ Learning-Process/src/test/java/in/learning/LearningProcess/controller/HelloWorldControllerTest.java
@@ -0,0 +1,12 @@
+package in.learning.LearningProcess.controller;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldControllerTest {
+
+	@Test
+	void helloWorldTest() {
+		System.out.println("hello-world implemented");
+	}
+
+}
