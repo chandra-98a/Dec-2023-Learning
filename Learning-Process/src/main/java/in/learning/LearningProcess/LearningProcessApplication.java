da720a0a80611d8c25524c718b5168d9e9fad17f
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-05 11:35:43 +0530

    "Created a project for Spring Boot Restful web services "

---
+++ Learning-Process/src/main/java/in/learning/LearningProcess/LearningProcessApplication.java
@@ -0,0 +1,13 @@
+package in.learning.LearningProcess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProcessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProcessApplication.class, args);
+	}
+
+}
---
+++ Learning-Process/src/test/java/in/learning/LearningProcess/LearningProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package in.learning.LearningProcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
