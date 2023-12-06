fb1b819da7b8a1298bb9d3e1e4a570b421a63d1e
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-06 11:09:10 +0530

    @PostMapping and @PutMapping

--- Learning-Process/src/main/java/in/learning/LearningProcess/controller/MentorController.java
+++ Learning-Process/src/main/java/in/learning/LearningProcess/controller/MentorController.java
@@ -3,9 +3,14 @@ package in.learning.LearningProcess.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.learning.LearningProcess.bean.Mentor;
@@ -39,10 +44,32 @@ public class MentorController {
 	}
 	
 	//REST API with @RequestParam
-	//http://localhost:8080/mentor/query?id=1
+	//http://localhost:8080/mentor/query?id=1&name=Vasantha
 	@GetMapping("/mentor/query")
-	public Mentor mentorRequestVariable(@RequestParam int id) {
-		return new Mentor(id,"Ashoka","ashok@gmail.com","IT team");
+	public Mentor mentorRequestVariable(@RequestParam int id,
+			@RequestParam String name) {
+		return new Mentor(id,name,"ashok@gmail.com","IT team");
 		
+	}
+	
+	//REST API with POST request
+	@PostMapping("/mentor")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Mentor createMentor(@RequestBody Mentor mentor) {
+		System.out.println(mentor.getId());
+		System.out.println(mentor.getName());
+		System.out.println(mentor.getEmail());
+		System.out.println(mentor.getRole());
+		return mentor;
+	}
+	
+	
+	//REST API with PUT request
+	@PutMapping("/mentor/{id}")
+	public Mentor updateMentor(@RequestBody Mentor mentor,@PathVariable int id) {
+		System.out.println(mentor.getName());
+		System.out.println(mentor.getEmail());
+		System.out.println(mentor.getRole());
+		return mentor;
 	}
 }
