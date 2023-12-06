38c4282cfa84053acd5f0c89f243bba129cdb56d
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-06 12:39:41 +0530

    Learning-Process 1

---
+++ Learning-Process-1/src/main/java/in/learning/LearningProcess1/LearningProcess1Application.java
@@ -0,0 +1,13 @@
+package in.learning.LearningProcess1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningProcess1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningProcess1Application.class, args);
+	}
+
+}
---
+++ Learning-Process-1/src/test/java/in/learning/LearningProcess1/LearningProcess1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.learning.LearningProcess1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningProcess1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
