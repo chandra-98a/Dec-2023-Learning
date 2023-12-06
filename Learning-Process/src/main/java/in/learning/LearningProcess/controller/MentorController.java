e95cef3ba4bd87b79b785b0ecc371220c61ae9af
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-06 11:51:40 +0530

    Using ResponseEntity class

--- Learning-Process/src/main/java/in/learning/LearningProcess/controller/MentorController.java
+++ Learning-Process/src/main/java/in/learning/LearningProcess/controller/MentorController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,65 +21,69 @@ import in.learning.LearningProcess.bean.Mentor;
 public class MentorController {
 
 	@GetMapping("/mentor")
-	public Mentor getMentor() {
+	public ResponseEntity<Mentor> getMentor() {
 		Mentor mentor=new Mentor(
 				1,"PaviPoovamma","pavi@gmail.com","Java Developer"
 				);
-		return mentor;
+		//return new ResponseEntity<>(mentor, HttpStatus.OK);
+		//return ResponseEntity.ok(mentor);
+		return ResponseEntity.ok().header("custom-header","Chandu").body(mentor);
 	}
 	
 	@GetMapping("/mentorsList")
-	public List<Mentor> getMentorList(){
+	public ResponseEntity<List<Mentor>> getMentorList(){
 		List<Mentor> mentor=new ArrayList<>();
 		mentor.add(new Mentor(1,"Naveen","naveen@gmail.com","Azure Team"));
 		mentor.add(new Mentor(2,"Sonu","sonu@gmail.com","AWS Architect"));
 		mentor.add(new Mentor(3,"Prasanna","prasu@gmail.com","Agile Developer"));
 		
-		return mentor;
+		return ResponseEntity.ok(mentor);
 	}
 	
 	//REST API with PathVariable  {id}=URI Template Variable
 	@GetMapping("/mentor/{id}/{name}")
-	public Mentor mentorPathVariable(@PathVariable("id") int mentorId,
+	public ResponseEntity<Mentor> mentorPathVariable(@PathVariable("id") int mentorId,
 			@PathVariable String name) {
-		return new Mentor(mentorId,name,"naveen@gmail.com","Azure Team");
+		Mentor mentor= new Mentor(mentorId,name,"naveen@gmail.com","Azure Team");
+		return ResponseEntity.ok(mentor);
 	}
 	
 	//REST API with @RequestParam
 	//http://localhost:8080/mentor/query?id=1&name=Vasantha
 	@GetMapping("/mentor/query")
-	public Mentor mentorRequestVariable(@RequestParam int id,
+	public ResponseEntity<Mentor> mentorRequestVariable(@RequestParam int id,
 			@RequestParam String name) {
-		return new Mentor(id,name,"ashok@gmail.com","IT team");
+		Mentor mentor= new Mentor(id,name,"ashok@gmail.com","IT team");
+		return ResponseEntity.ok(mentor);
 		
 	}
 	
 	//REST API with POST request
 	@PostMapping("/mentor")
-	@ResponseStatus(HttpStatus.CREATED)
-	public Mentor createMentor(@RequestBody Mentor mentor) {
+	//@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Mentor> createMentor(@RequestBody Mentor mentor) {
 		System.out.println(mentor.getId());
 		System.out.println(mentor.getName());
 		System.out.println(mentor.getEmail());
 		System.out.println(mentor.getRole());
-		return mentor;
+		return new ResponseEntity<>(mentor,HttpStatus.CREATED);
 	}
 	
 	
 	//REST API with PUT request
 	@PutMapping("/mentor/{id}")
-	public Mentor updateMentor(@RequestBody Mentor mentor,@PathVariable int id) {
+	public ResponseEntity<Mentor> updateMentor(@RequestBody Mentor mentor,@PathVariable int id) {
 		System.out.println(mentor.getName());
 		System.out.println(mentor.getEmail());
 		System.out.println(mentor.getRole());
-		return mentor;
+		return new ResponseEntity<>(mentor,HttpStatus.OK);
 	}
 	
 	//REST API with PathVariable  {id}=URI Template Variable Delete request
 	
 	@DeleteMapping("/mentor/{id}")
-	public String deleteMapping(@PathVariable int id) {
+	public ResponseEntity<String> deleteMapping(@PathVariable int id) {
 		System.out.println(id);
-		return "Mentor delete successfully";
+		return ResponseEntity.ok("Mentor delete successfully");
 	}
 }
