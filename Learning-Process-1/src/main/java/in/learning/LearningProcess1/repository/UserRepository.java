c96e70c0ce118d4e5d05cbca6e8886213130cbbb
chandra-98a <chandrakalaa2022@gmail.com>
2023-12-06 13:18:59 +0530

    UserRepository

---
+++ Learning-Process-1/src/main/java/in/learning/LearningProcess1/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package in.learning.LearningProcess1.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.learning.LearningProcess1.entity.User;
+
+public interface UserRepository  extends JpaRepository<User, Integer>{
+
+}
+
